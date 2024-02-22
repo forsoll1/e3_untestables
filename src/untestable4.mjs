d86de2b8af06f9e0c519df59ac9943ab8af03ede
Olli Forssell <olli20112@student.hamk.fi>
2024-02-22 17:27:16 +0200

    done

--- src/untestable4.mjs
+++ src/untestable4.mjs
@@ -1,7 +1,15 @@
 import argon2 from "@node-rs/argon2";
 import pg from "pg";
 
-export class PostgresUserDao {
+/* 
+The postgres connection pool configuration isn't accessible for testing so we need to be able to pass the pool config data to the postgresUserDao as an argument at initialization. 
+The class also uses the singleton pattern which is potentially difficult to test. We will remove this pattern and instead simply create a new instance. 
+Wheter or not the removal of the singleton pattern is a good idea will of course depend on how the class is used and how its singleton-like nature is communicated
+across the system so that no unneccesary instances are created. 
+Because the postgresUserDao class object will no longer be accessible as a static class property, we will pass it to the PasswordService-class's constructor as an argument. 
+*/
+
+export class oldPostgresUserDao {
   static instance;
 
   static getInstance() {
@@ -48,8 +56,66 @@ export class PostgresUserDao {
   }
 }
 
-export class PasswordService {
+export class oldPasswordService {
   users = PostgresUserDao.getInstance();
+
+  async changePassword(userId, oldPassword, newPassword) {
+    const user = await this.users.getById(userId);
+    if (!argon2.verifySync(user.passwordHash, oldPassword)) {
+      throw new Error("wrong old password");
+    }
+    user.passwordHash = argon2.hashSync(newPassword);
+    await this.users.save(user);
+  }
+}
+
+export class PostgresUserDao {
+
+  db;
+
+  constructor(poolConfig){
+    this.db = this.createPostgresPool(poolConfig)
+  }
+
+  createPostgresPool(poolConfig){
+    return new pg.Pool(poolConfig)
+  }
+
+  close() {
+    this.db.end();
+  }
+
+  #rowToUser(row) {
+    return { userId: row.user_id, passwordHash: row.password_hash };
+  }
+
+  async getById(userId) {
+    const { rows } = await this.db.query(
+      `select user_id, password_hash
+       from users
+       where user_id = $1`,
+      [userId]
+    );
+    return rows.map(this.#rowToUser)[0] || null;
+  }
+
+  async save(user) {
+    await this.db.query(
+      `insert into users (user_id, password_hash)
+       values ($1, $2)
+       on conflict (user_id) do update
+           set password_hash = excluded.password_hash`,
+      [user.userId, user.passwordHash]
+    );
+  }
+}
+
+export class PasswordService {
+  users;
+
+  constructor(postGresService){
+    this.users = postGresService
+  }
 
   async changePassword(userId, oldPassword, newPassword) {
     const user = await this.users.getById(userId);
--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -1,17 +1,65 @@
 import { afterEach, beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
 import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
+const fs = require('fs')
+import argon2 from "@node-rs/argon2";
 
 describe("Untestable 4: enterprise application", () => {
-  let service;
-  beforeEach(() => {
-    service = new PasswordService();
+  let postgresService;
+  let passwordService;
+  let testPeople = [[1, "password1"], [2, argon2.hashSync("password2")]]
+  let createTables = fs.readFileSync('./src/create-tables.sql', 'utf-8')
+  let dropTables = fs.readFileSync('./src/drop-tables.sql', 'utf-8')
+  let poolData = {user: 'untestable', host: '127.0.0.1', port:5432, password:'secret', database:'db'}
+
+  beforeEach(async () => {
+    passwordService = new PasswordService(new PostgresUserDao(poolData));
+    postgresService = passwordService.users
+    try {
+      await postgresService.db.query(createTables)
+    } catch (error) {console.log(error)}
+
+    await postgresService.db.query('TRUNCATE users')
+    
+    for (const p of testPeople) {
+      await postgresService.db.query({
+        text:'INSERT INTO users(user_id, password_hash) VALUES($1, $2)',
+        values: [p[0], p[1]]
+      })
+    }    
+
   });
 
-  afterEach(() => {
-    PostgresUserDao.getInstance().close();
+  afterEach(async () => {
+    await postgresService.db.query(dropTables)
+    postgresService.close();
   });
 
-  test("todo", async () => {
-    // TODO: write proper tests for both PasswordService and PostgresUserDao
+  test("PostgresUserDao can return db rows", async () => {
+    let fetchPerson = await postgresService.getById(1)
+    expect(fetchPerson).to.deep.equal({userId: 1, passwordHash: "password1"})
+  });
+
+  test("PostgresUserDao can change password", async () => {
+    let user = {userId: 1, passwordHash: 'changedpassword'}
+    await postgresService.save(user)
+    let fetchPerson = await postgresService.getById(1)
+    expect(fetchPerson.passwordHash).to.equal('changedpassword')
+  });
+
+  test("PasswordService can change password", async () => {
+    await passwordService.changePassword(2, 'password2', 'changedpassword')
+    let fetchPerson = await postgresService.getById(2)
+    expect(argon2.verifySync(fetchPerson.passwordHash, 'changedpassword')).to.equal(true)
+  });
+
+  test("PasswordService won't change password if old password is wrong", async () => {
+    let err = null
+    try {
+      await passwordService.changePassword(2, 'WRONGPASSWORD', 'changedpassword')
+    } catch (error) {
+      err = error
+    }
+    expect(err.message).to.equal("wrong old password")
   });
 });
